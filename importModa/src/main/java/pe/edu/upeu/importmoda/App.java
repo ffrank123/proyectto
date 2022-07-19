21c20fbdaf821e34f0a5ecb7d92e62c94eb9cc12
ffrank123 <fraserk599@gmail.com>
2022-07-18 22:03:20 -0500

    asdf

---
+++ importModa/src/main/java/pe/edu/upeu/importmoda/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.importmoda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      new MenuOpciones().menuPrincipal();
+    }
+}
